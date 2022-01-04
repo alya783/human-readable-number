27a575884fc45702b118987edbd6720d55bfb6be
alya783 <76521831+alya783@users.noreply.github.com>
2022-01-04 11:41:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,107 +26,27 @@ module.exports = function toReadable (number) {
         90: 'ninety',
     }
         
-    for(var k in numbers){
-        if(+k === number){
-           return numbers[k];
-        } else if(number > 13 && number < 20){
-            let numArr = number.toString().split('');
-            if(+k === +numArr[1]){
-				if(numbers[k].charAt(numbers[k].length - 1) === 't'){
-					return numbers[k] + 'een';
-				}
-               return numbers[k] + 'teen';
-            }
-        } else if(number > 20 && number < 100){
-            let numArr2 = [];
-            while(number > 0){
-                for(let i = 1; number > 0; i *= 10){
-                    let n = (number % 10) * i;
-                    numArr2.unshift(n);
-                    number = parseInt(number / 10);
-                }
-            }
-			
-			let first = ' ';
-			let second = ' ';
-
-			for (var k in numbers){
-
-				if((+k === numArr2[0]) && (numArr2[0] % 10 === 0) && numArr2[1] === 0){
-					return numbers[k];
-            	}
-				
-				if( +k === numArr2[0]){
-					first = numbers[k];
-            	}
-
-				if( +k === numArr2[1]){
-					second = numbers[k];
-            	}
-			}
-            
-			return (first + " " + second);	
-	
-    } else if(number >= 100 && number < 1000){
-		let separateNum = number.toString().split('');  
-        let numArr3 = [separateNum[0], separateNum[1] + separateNum[2]];	
-		if(+k === +numArr3[0] && (number % 100 === 0)){
-            return numbers[k] + ' hundred';
-        } else if(number % 100 !== 0){
-		
-		let fir = ' ';
-		let sec = ' ';
-
-		if (+numArr3[1] < 20){
-		for (var k in numbers){
-			if( +k === +numArr3[0]){
-				fir = numbers[k] + ' hundred ';
-        	}
-
-			if( +k === +numArr3[1]){
-				sec = numbers[k];
-            }else{
-				let arr = (+numArr3[1]).toString().split('');
-				if(+k === +arr[1]){
-               	   sec = numbers[k] + 'teen';
-            	}
-			}	
-       	 }	
-			return fir + '' + sec;
-		}
-        
-        let integer = +numArr3[1];
-        let arr1 = [];
-        while(integer > 0){
-        for(let j = 1; integer > 0; j *= 10){
-                let p = (integer % 10) * j;
-                arr1.unshift(p);
-                integer = parseInt(integer / 10);
-            }
+   if(Object.keys(numbers).includes(String(number))){
+        return numbers[number];
+    } else if(number >= 14 && number <= 19){
+    	let keyNum = number % 10;
+    	return numbers[keyNum] + 'teen';
+    } else if(number >= 20 && number <= 99){
+    	let ost = number % 10;
+        let decimalPart = number - ost;
+        return numbers[decimalPart] + " " + numbers[ost];   
+    } else if(number >= 100 && number <= 999){
+        let hundredPart;
+        if(number % 100 === 0){
+      	    hundredPart = number / 100;
+            return numbers[hundredPart] + " hundred";
+        } else{
+      	    let ost2 = number % 100;
+      	    hundredPart = (number - ost2) / 100;
+      	    return numbers[hundredPart] + " hundred " + toReadable(ost2);
         }
-					
-        let int1 = '';
-        let int2 = '';
-                    
-        for (var k in numbers){
-			if( +k === +numArr3[0]){
-				fir = numbers[k] + ' hundred';
-        	}
-						
-			if(+k === arr1[0]){
-                int1 = numbers[k];
-            }
-
-			if(arr1[1] === 0) continue;
-						
-            if(+k === arr1[1]){
-                int2 = numbers[k];
-            } 			
-		}
-				
-        sec = int1 + " " + int2;
-		return (fir +" "+ sec);
-	  }
-	}
-  }
+    	
+    } else{
+        console.error(`This case ${number} is not included`);
+    }
 }
